89156e76ff7452ac6c4bd08d938d56f900e286f4
nickqq11 <nickaids@gmail.com>
2023-11-20 16:39:56 +1030

    temp

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,20 @@
 /*   By: nhuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 10:22:09 by nhuang            #+#    #+#             */
-/*   Updated: 2023/08/30 09:35:58 by nhuang           ###   ########.fr       */
+/*   Updated: 2023/11/20 16:05:47 by nhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdio.h>
-#include <unistd.h>
+# include <stdio.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+char	get_next_line(int fd);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,14 +6,14 @@
 /*   By: nhuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 10:21:57 by nhuang            #+#    #+#             */
-/*   Updated: 2023/11/02 15:57:44 by nhuang           ###   ########.fr       */
+/*   Updated: 2023/11/20 15:26:00 by nhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char *get_next_line(int fd)
 {
-	char	*readline;
-	char	*buffer;
+	char		*readline;
+	char		*buffer;
 	static char	*toread;
 
 	if((fd < 0) || (BUFFER_SIZE <= 0))
