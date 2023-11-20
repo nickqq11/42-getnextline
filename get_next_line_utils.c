f28f61463a4c35b95d9be87855229807900195ce
nickqq11 <118785245+nickqq11@users.noreply.github.com>
2023-11-20 15:17:34 +1030

    try

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,16 @@
 /*   By: nhuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 10:21:57 by nhuang            #+#    #+#             */
-/*   Updated: 2023/07/20 10:21:59 by nhuang           ###   ########.fr       */
+/*   Updated: 2023/11/02 15:57:44 by nhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char *get_next_line(int fd)
+{
+	char	*readline;
+	char	*buffer;
+	static char	*toread;
+
+	if((fd < 0) || (BUFFER_SIZE <= 0))
+		return (NULL);
+}
