7550bc44baf631ccde606fb68ca847eda49b0ac7
nickqq11 <nickaids@gmail.com>
2023-11-20 19:01:52 +1030

    ha

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,26 @@
 /*   By: nhuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/20 10:22:05 by nhuang            #+#    #+#             */
-/*   Updated: 2023/08/30 08:33:55 by nhuang           ###   ########.fr       */
+/*   Updated: 2023/11/20 18:09:50 by nhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char get_next_line(int fd)
+char	get_next_line(int fd)
 {
+	char		*theline;
+	char		*buffer;
+	static char	*anchor;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = malloc (sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	theline = reader(fd, buffer, anchor);
+	if (!theline)
+		return (NULL);
+	
 	
 }
